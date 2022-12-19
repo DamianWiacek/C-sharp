e56d44851f8ee060f6dafc1dca6be3f0ce2a6e56
DamianWiacek <109426665+DamianWiacek@users.noreply.github.com>
2022-12-19 16:12:07 +0100

    Add files via upload

---
+++ FirstNon-RepeatingCharacter/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+
+public class Kata
+{
+    public static string FirstNonRepeatingLetter(string s)
+    {
+        string wynik = "";
+        foreach (var item in s)
+        {
+            if (s.Count(f => f.ToString().ToLower() == item.ToString().ToLower()) == 1)
+            {
+                wynik = item.ToString();
+                break;
+            }
+        }
+        return wynik;
+    }
+}
