8d1b6ff7c83ade3552f98c73d35231668f5ce3d1
DamianWiacek <109426665+DamianWiacek@users.noreply.github.com>
2022-12-19 16:20:17 +0100

    Add files via upload

---
+++ IntToIp/Program.cs
@@ -0,0 +1,34 @@
+﻿
+public class Kata
+{
+
+    public static string UInt32ToIP(uint ip)
+    {
+        string temp = "";
+        string result = "";
+        for (int i = 31; i >= 0; i--)
+        {
+            if (ip >= Math.Pow(2, i))
+            {
+                ip -= (uint)Math.Pow(2, i);
+                temp += 1;
+            }
+            else
+            {
+                temp += 0;
+            }
+        }
+        for (int i = 0; i < 32; i += 8)
+        {
+            result += Convert.ToInt32(temp.Substring(i, 8), 2).ToString() + ".";
+        }
+        return result.Substring(0, result.Length - 1);
+    }
+
+    public static void Main(string[] args)
+    {
+
+
+        Console.WriteLine(Kata.UInt32ToIP(2149583361));
+    }
+}
