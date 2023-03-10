90b9df80ba00db616d1182730d86bfd61e1c7ded
DamianWiacek <109426665+DamianWiacek@users.noreply.github.com>
2023-03-10 13:55:03 +0100

    Add files via upload

---
+++ Panagram/Panagram.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Linq;
+
+public static class Panagram
+{
+    //this program check if string contains every letter in alphabet
+    public static bool IsPangram(string str)
+    {
+        var wynik = str.Trim().ToLower().ToList();
+        return wynik.Distinct().Count() >= 26 ? true : false;
+    }
+}
